8770217bdad761bdbeb6d70ad28e967dc66ed2df
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-02-09 16:30:36 -0300

    Requisito 1: Implementa as funções getCategories e getProductsFromCategoryAndQuery

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,20 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL_API = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const dataCategories = await fetch(URL_API);
+  const data = dataCategories.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
+  if (!categoryId && !query) return [];
+  const URL_API = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const dataProducts = await fetch(URL_API);
+  const data = await dataProducts.json();
+  return data;
 }
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+
 }
